54f81764b3c027cd6d3c8001eaa40e835e7f3ee7
Daria <n-d-p@mail.ru>
2024-07-02 20:57:08 +0300

    ++ button "try again"

--- shoe-store/src/components/lists/catalog/catalogList.tsx
+++ shoe-store/src/components/lists/catalog/catalogList.tsx
@@ -1,17 +1,27 @@
 import List from "../list.tsx";
-import {useAppSelector} from "../../../redux/hooks.ts";
+import {useAppDispatch, useAppSelector} from "../../../redux/hooks.ts";
+import {fetchCatalogList} from "../../../redux/slices/catalogListSlice.ts";
+import {Button} from "react-bootstrap";
 
 export default function CatalogList() {
+    const dispatch = useAppDispatch()
 
-    const {catalogList, searchStr, activeCategory} = useAppSelector(state => state.catalogList)
+    const {catalogList, searchStr, activeCategory,loadingListError} = useAppSelector(state => state.catalogList)
 
+    const errorLoading = <div className="small-block">
+        <h4 className="text-center text-secondary m-5">Упс! Что-то пошло не так.</h4>
+        <Button variant="secondary" className="w-25 m-auto" onClick={() => dispatch(fetchCatalogList())}>
+            Попробовать еще раз</Button>
+    </div>
+
+    const notFoundItems = <div className="small-block">
+        <h4 className="text-center text-secondary m-5">Упс! Поиск не дал результатов.</h4>
+    </div>
     return (
         <>
             {catalogList.length > 0 ?
-                <List items={catalogList}/> : searchStr || activeCategory ?
-                    <div className="small-block">
-                        <h4 className="text-center text-secondary m-5">Упс! Поиск не дал результатов.</h4>
-                    </div> :
+                <List items={catalogList}/> : loadingListError ? errorLoading : searchStr || activeCategory ?
+                    notFoundItems :
                     null}
         </>
     )
